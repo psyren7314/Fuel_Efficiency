246ed2c04184bebe0e48d0dd88cd102d08a32e76
psyren7314 <84650350+psyren7314@users.noreply.github.com>
2026-06-20 08:57:02 +0900

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+// ==========================================
+// 【Supabase接続設定ファイル】
+// ==========================================
+
+// ご自身のSupabase接続情報をそれぞれの ` の間に貼り付けてください。
+// 前後の `（バッククォート）は消さないようご注意ください。
+
+const SUPABASE_CONFIG_URL = `https://jpdpoxrwugqeobkbyhzw.supabase.co`.trim(); 
+
+const SUPABASE_CONFIG_ANON_KEY = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpwZHBveHJ3dWdxZW9ia2J5aHp3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE2MTQyNTUsImV4cCI6MjA5NzE5MDI1NX0.6bmXeAWZ5bZVa7oPT6upM3_CpXoHIeU8fIYwGiT9scw`.trim().replace(/\s+/g, '');
