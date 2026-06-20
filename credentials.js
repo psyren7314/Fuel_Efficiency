e64c552fa12f99b9a23bf309f018102c69ca3b71
psyren7314 <84650350+psyren7314@users.noreply.github.com>
2026-06-20 22:12:03 +0900

    Update credentials.js

--- credentials.js
+++ credentials.js
@@ -9,3 +9,8 @@ const ADMIN_ID = "u7Zne2rH";
 // ※初期パスワードは「FHQqTiRdun2G7ExQ」に設定されています。
 // （「password」をSHA-256でハッシュ化した文字列が以下です）
 const ADMIN_PW_HASH = "13725ecf4561211d6582eebcde62a3ffa6026bf53104d014433277863e4e8dea";
+
+// --- 2. 一般ユーザー用 (メインページの閲覧・入力用) ---
+const USER_ID = "tL4jzJsB";
+// パスワード: bytpDkCUe6E24ZXP （ハッシュ化前の文字列）
+const USER_PW_HASH = "de552f4158c669f3d0e34f6bc3051c2a29ee3d9628f4bd24ca9e4a1e94d4ffce";
